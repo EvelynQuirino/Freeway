78a2dcf7ef0dd9ad59d1829d55615f2214b44671
Evelyn <clara.eqs@hotmail.com>
2022-01-02 20:40:11 -0300

    first commit

---
+++ carro.js
@@ -0,0 +1,51 @@
+
+//código do carro
+
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [40, 96, 150, 210, 270, 318]; 
+let velocidadeCarros = [3, 3.6, 4.3, 5, 3.9, 4.7];
+let comprimentoCarro = 50;
+let alturaCarro = 30;
+
+
+function mostraCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  }
+}
+
+function movimentaCarro(){
+    for(let i = 0; i < imagemCarros.length; i++){
+        xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+    for(let i = 0; i < imagemCarros.length; i++){
+        if(passouTodaATela(xCarros[i])){
+                xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaATela(xCarros){
+    return xCarros < - 50;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
